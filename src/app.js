73d6f40d2dfac59a11eeb2c216f5f3eb4a18a20a
ana <68268688+Angie192020@users.noreply.github.com>
2025-06-25 18:37:00 +0000

    simplificado_uwu

--- src/app.js
+++ src/app.js
@@ -7,43 +7,49 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
 
 // index =       0        1        2          3        4
 const juego = ["rock", " paper", "scissor", "lizard", "spock"]
-const obtenerunparametro = () => {
+const obtener_un_parametro = () => {
     const index = Math.floor(Math.random() * juego.length) // el index no es necasario que se llame así
     return juego[index]
 }
 const compara = (decision_usuario) => {
-    const originalparam = obtenerunparametro()
+    const original_param = obtener_un_parametro()
     console.log(`Decides ${decision_usuario}`);
-    console.log(`Ha salido ${originalparam}`)
-    if (decision_usuario == originalparam) {
-        console.log("Empate")
-    } else if (decision_usuario == "rock" && (originalparam == "scissors" ||  originalparam == "lizard")) {
-        console.log("Enhorabuena, lo aplataste")
-    } else if (decision_usuario == "paper" && (originalparam == "rock")) {
-        console.log("Enhorabuena, has cubierto a rock")
-    } else if (decision_usuario == "paper" && (originalparam == "spock")) {
-        console.log("Enhorabuena, desautorizaste a spock")
-    } else if (decision_usuario == "scissors" && (originalparam == "paper")) {
-        console.log("Enhorabuena has cortado a paper")
-    } else if (decision_usuario == "scissors" && (originalparam == "lizard")) {
-        console.log("Enhorabuena has decapacitado a lizard")
-    } else if (decision_usuario == "lizard" && (originalparam == "paper")) {
-        console.log("Enhorabuena devoraste a paper")
-    } else if (decision_usuario == "lizard" && (originalparam == "spock")) {
-        console.log("Enhorabuena envenenaste a spock")
-    } else if (decision_usuario == "spock" && (originalparam == "scissors")) {
-        console.log("Enhorabuena has roto a scissors")
-    } else if (decision_usuario == "spock" && (originalparam == "rock")) {
-        console.log("Enhorabuena has vaporizado a rock")
-    } else {
-        console.log("Has perdido")
+    console.log(`Ha salido ${original_param}`)
+    if (decision_usuario == original_param) {
+        return console.log("Empate")
     }
-    
+    if (decision_usuario == "rock" && (original_param == "scissors" ||  original_param == "lizard")) {
+        return console.log("Enhorabuena, lo aplataste")
+    }
+    if (decision_usuario == "paper" && (original_param == "rock")) {
+        return console.log("Enhorabuena, has cubierto a rock")
+    }
+    if (decision_usuario == "paper" && (original_param == "spock")) {
+        return console.log("Enhorabuena, desautorizaste a spock")
+    }
+    if (decision_usuario == "scissors" && (original_param == "paper")) {
+        return console.log("Enhorabuena has cortado a paper")
+    }
+    if (decision_usuario == "scissors" && (original_param == "lizard")) {
+        return console.log("Enhorabuena has decapitado a lizard")
+    }
+    if (decision_usuario == "lizard" && (original_param == "paper")) {
+        return console.log("Enhorabuena devoraste a paper")
+    }
+    if (decision_usuario == "lizard" && (original_param == "spock")) {
+        return console.log("Enhorabuena envenenaste a spock")
+    }
+    if (decision_usuario == "spock" && (original_param == "scissors")) {
+        return console.log("Enhorabuena has roto a scissors")
+    }
+    if (decision_usuario == "spock" && (original_param == "rock")) {
+        return console.log("Enhorabuena has vaporizado a rock")
+    }
+        console.log("Has perdido")
 }
 const adivinanza = prompt("opcion: rock, paper, scissors, lizard or spock.").toLowerCase();
     compara(adivinanza);
